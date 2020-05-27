231b0d1955e30edeaf8d91e2ed002b00ad67c345
jackcolin21307 <jackcolin21307@gmail.com>
2020-05-26 22:02:37 -0400

    feat: seding challenge node-typescript

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,20 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransationDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface GetTransactionAndBalanceDTO {
+  transactions: Transaction[];
+  balance: Balance;
 }
 
 class TransactionsRepository {
@@ -13,16 +24,49 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): GetTransactionAndBalanceDTO {
+    const balance = this.getBalance();
+
+    const transactions = {
+      transactions: this.transactions,
+      balance,
+    };
+    return transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((totalIncome, nextTransaction) => {
+      return nextTransaction.type === 'income'
+        ? totalIncome + nextTransaction.value
+        : totalIncome;
+    }, 0);
+
+    const outcome = this.transactions.reduce(
+      (totalOutcome, nextTransaction) => {
+        return nextTransaction.type === 'outcome'
+          ? totalOutcome + nextTransaction.value
+          : totalOutcome;
+      },
+      0,
+    );
+
+    const total = income - outcome;
+
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransationDTO): Transaction {
+    const transation = new Transaction({ title, value, type });
+
+    this.transactions.push(transation);
+
+    return transation;
   }
 }
 
